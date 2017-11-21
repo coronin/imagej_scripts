bf1dd31f4f5f688799bf621a5cba42f3728d9c26
Liang C <liang.cai@me.com>
2017-11-21 13:53:31 +0800

    xls almost done

--- branching/otsuOtsu-2.py
+++ branching/otsuOtsu-2.py
@@ -6,14 +6,16 @@ import os
 import csv
 import time
 
-folder = '/Users/liang/Downloads/wj/'  # remeber to have / at the end on mac
+
+folder = './'
+# folder = '/Users/liang/Downloads/wj/'  # remeber to have / at the end on mac
 positiveG = 199.99  # above which view as GFP positive
 
 # use file names to generate unique tag list
 files = [f.replace('-Results.csv', '') for f in os.listdir(folder) if f.endswith('-Results.csv')]
-timestamp = int(time.time())
-if not os.path.exists(folder + '%d/' % timestamp):
-    os.makedirs(folder + '%d/' % timestamp)
+ts = int(time.time())
+if not os.path.exists(folder + '%d/' % ts):
+    os.makedirs(folder + '%d/' % ts)
 
 
 def data_from_csv(filename):
@@ -30,7 +32,9 @@ def data_from_csv(filename):
 
 
 # for loop
-fields = ['G Mean', 'G outsideMean', 'B Mean', 'R Mean', 'Roo Count', 'Roo Mean', 'Roo Average Size', 'Roo Total Area', 'Roo %Area', 'nuc Area', 'ID', 'nuc X,Y,W,H']
+fields = ['G Mean', 'G outsideMean', 'B Mean', 'R Mean', 'Roo Count',
+          'Roo Mean', 'Roo Average Size', 'Roo Total Area', 'Roo %Area',
+          'nuc Area', 'nuc X Y WxH', 'file tag', 'ID']
 dHigh = []
 dLow = []
 for file in files:
@@ -49,13 +53,13 @@ for file in files:
                 lineData['ID'] = str(int(oo[y][2:]) + 1)
                 continue
             lineData['Roo %s' % a] = oo[y]
+        lineData['file tag'] = file[:-5]
         # ID Label Area Mean Min Max BX BY Width Height
         #     G-Mean G-Percentage G-outsideMean
         lineData['nuc Area'] = oData[x][2]
         lineData['B Mean'] = oData[x][3]
-        lineData['nuc X,Y,W,H'] = '%s,%s,%s,%s' % (
-            oData[x][6].replace(',', ''), oData[x][7].replace(',', ''),
-            oData[x][8].replace(',', ''), oData[x][9].replace(',', ''))
+        lineData['nuc X Y WxH'] = '%s %s %sx%s' % (
+            oData[x][6], oData[x][7], oData[x][8], oData[x][9])
         lineData['G Mean'] = oData[x][10]
         lineData['G outsideMean'] = oData[x][12]
         lineData['R Mean'] = oData[x + len(ooData)][3]
@@ -70,25 +74,23 @@ for file in files:
                     lineData['ID'],
                     lineData['G Mean'], lineData['G outsideMean'])
             d = [lineData] + d
-            lineData['file tag'] = file[:-5]
             dHigh.append(lineData)
         else:
             d.append(lineData)
-            lineData['file tag'] = file[:-5]
             dLow.append(lineData)
-    with open(folder + '%d/' % timestamp + file[:-5] + '-%d.csv' % timestamp, 'w') as f:
+    with open(folder + '%d/' % ts + file[:-5] + '-%d.csv' % ts, 'w') as f:
         writer = csv.DictWriter(f, fieldnames=fields)
         writer.writeheader()
         for dd in d:
             writer.writerow(dd)
 
-with open(folder + '%d/' % timestamp + 'GFP-high-%d.csv' % timestamp, 'w') as f:
-    writer = csv.DictWriter(f, fieldnames = fields + ['file tag'])
+with open(folder + '%d/' % ts + 'GFP-high-%d.csv' % ts, 'w') as f:
+    writer = csv.DictWriter(f, fieldnames=fields)
     writer.writeheader()
     for dd in dHigh:
         writer.writerow(dd)
-with open(folder + '%d/' % timestamp + 'GFP-low-%d.csv' % timestamp, 'w') as f:
-    writer = csv.DictWriter(f, fieldnames = fields + ['file tag'])
+with open(folder + '%d/' % ts + 'GFP-low-%d.csv' % ts, 'w') as f:
+    writer = csv.DictWriter(f, fieldnames=fields)
     writer.writeheader()
     for dd in dLow:
         writer.writerow(dd)
